00ebf3e729a95a849d192a160f17c37125990f2a
Brett Viren <bv@bnl.gov>
2016-06-15 10:30:17 -0400

    Understand scale.

--- python/larf/plot.py
+++ python/larf/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 
-def slice(arr, outfile, title="BEM Calculation (potential)", ngridx=150, ngridy=150, xrange=(50,50), yrange=(50,50), **kwds):
+def slice(arr, outfile, title="BEM Calculation (potential)", ngridx=150, ngridy=150, xrange=(50,50), yrange=(50,50), limits=None, **kwds):
     
     # Plot the image
     matplotlib.rcParams['figure.figsize'] = (5.0, 4.0) # Adjust the figure size in IPython
@@ -10,6 +10,8 @@ def slice(arr, outfile, title="BEM Calculation (potential)", ngridx=150, ngridy=
     if kwds.get('log',False):
         toplot = np.log10(toplot)
     plt.imshow(toplot, extent=(xrange[0],xrange[1], yrange[0],yrange[1]), origin='lower')
+    if limits:
+        plt.clim(*limits)
     plt.colorbar()
     plt.title(title)
     plt.savefig(outfile)
